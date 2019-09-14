c305e99fb7651058bb1adad210922d0cd84c04d1
Riyah1295 <nursyazriyah95@gmail.com>
2019-09-14 22:16:40 +0800

    Add files via upload

--- src/com/shoppingcart/entity/CartProduct.java
+++ src/com/shoppingcart/entity/CartProduct.java
@@ -2,29 +2,108 @@ package com.shoppingcart.entity;
 
 public class CartProduct {
 	
-	Cart cart;
-	Product product;
 	
-	public CartProduct(Cart cart, Product product) {
-		super();
-		this.cart = cart;
-		this.product = product;
+	private String user, product_name,product_desc;
+	private int cartId, quantity;
+	private double total_Quantity_Price, product_price;
+	
+	
+
+	public CartProduct(int cartId, String user, String product_name, String product_desc, double product_price ,
+		int quantity, double total_Quantity_Price ) {
+		this.user = user;
+		this.product_name = product_name;
+		this.product_desc = product_desc;
+		this.cartId = cartId;
+		this.quantity = quantity;
+		this.total_Quantity_Price = total_Quantity_Price;
+		this.product_price = product_price;
 	}
 
-	public Cart getCart() {
-		return cart;
+
+
+	public String getUser() {
+		return user;
 	}
 
-	public void setCart(Cart cart) {
-		this.cart = cart;
+
+
+	public void setUser(String user) {
+		this.user = user;
 	}
 
-	public Product getProduct() {
-		return product;
+
+
+	public String getProduct_name() {
+		return product_name;
 	}
 
-	public void setProduct(Product product) {
-		this.product = product;
+
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
 	}
+
+
+
+	public String getProduct_desc() {
+		return product_desc;
+	}
+
+
+
+	public void setProduct_desc(String product_desc) {
+		this.product_desc = product_desc;
+	}
+
+
+
+	public int getCartId() {
+		return cartId;
+	}
+
+
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+
+	public double getTotal_Quantity_Price() {
+		return total_Quantity_Price;
+	}
+
+
+
+	public void setTotal_Quantity_Price(double total_Quantity_Price) {
+		this.total_Quantity_Price = total_Quantity_Price;
+	}
+
+
+
+	public double getProduct_price() {
+		return product_price;
+	}
+
+
+
+	public void setProduct_price(double product_price) {
+		this.product_price = product_price;
+	}
+
+
 	
 }
