ad7f689d1375e977ba90a7541a67dfa521493256
Riyah1295 <nursyazriyah95@gmail.com>
2019-09-14 10:44:45 +0800

    Add files via upload

--- src/com/shoppingcart/dataUtil/UserDataUtil.java
+++ src/com/shoppingcart/dataUtil/UserDataUtil.java
@@ -5,10 +5,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import com.shoppingcart.entity.Product;
 import com.shoppingcart.entity.User;
 
 public class UserDataUtil {
@@ -41,7 +44,7 @@ public class UserDataUtil {
 		PreparedStatement pstmt = null;
 		try {
 			con = dataSource.getConnection();
-			String query = "INSERT INTO `shopping`.`user` (`Username`, `Password`, `Firstname`, `Lastname`, `Email`, `Gender`, `Dob`, `Address`, `Contact`)"
+			String query = "INSERT INTO shopping.user (Username, Password, Firstname, Lastname, Email, Gender, Dob, Address, Contact)"
 					+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, theUser.getUsername());
@@ -62,5 +65,70 @@ public class UserDataUtil {
 		}
 		
 	}
-
+	public User viewUserProfile(String user_username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public User loginUser(String input_email, String input_password) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rst = null;
+		User theUser = null;
+		try {
+			con = dataSource.getConnection();
+			String query = "SELECT * FROM user WHERE Username = ? AND Password=?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, input_email);
+			pstmt.setString(2, input_password);
+			rst = pstmt.executeQuery();
+			if(rst.next()) { // this is only returning 1 row; the registered user
+				theUser = new User();
+				theUser.setFirst_name(rst.getString("Firstname"));
+				theUser.setLast_name(rst.getString("Lastname"));
+				theUser.setEmail(rst.getString("Email"));
+				theUser.setGender(rst.getString("Gender"));
+				theUser.setDate_of_birth(rst.getString("Dob"));
+				theUser.setAddress(rst.getString("Address"));
+				theUser.setContact(rst.getString("Contact"));
+				System.out.println("profile retrieve done.");
+			}
+			
+			return theUser;
+		}finally {
+		close(con, pstmt, rst);
+		}
+	}
+	
+	public List<Product> getProducts() throws SQLException {
+		List<Product> products = new ArrayList<>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rst = null;
+		
+		try {
+			con = dataSource.getConnection();
+			
+			String qry="select * from shopping.product";
+			
+			stmt=con.prepareStatement(qry);
+			
+			rst=stmt.executeQuery(qry);
+			
+			while(rst.next()) {
+				int product_Id= rst.getInt("Product_Id");
+				String product_Name= rst.getString("Product_Name");
+				String product_Desc= rst.getString("Product_Desc");
+				double product_Price= rst.getInt("Product_Price");
+				
+				
+				Product tempProduct = new Product(product_Id, product_Name, product_Desc, product_Price);
+				
+				products.add(tempProduct);
+			}
+			return products;
+		}
+		finally {
+			close(con,stmt,rst);
+		}
+	}
 }
